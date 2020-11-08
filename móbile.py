95840aeeb184107a16056c393c5723949e43fce1
melovit <vitoramelo0503@gmail.com>
2020-11-08 17:04:07 -0300

    Adiciona a questão Móbile

---
+++ móbile.py
@@ -0,0 +1,11 @@
+a = int (input("Adionar valor de A:"))
+b = int (input("Adionar valor de B:"))
+c = int (input("Adionar valor de C:"))
+d = int (input("Adionar valor de D:"))
+
+if ((a == (b+c+d)) and ((b+c) == d) and (b == c)):
+    print ("S")
+else:
+    print ("N")
+    
+
